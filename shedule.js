898f2837ed467d9bb824713daed0e94deeae21ed
i.bikmaev <koshakk5@gmail.com>
2020-06-04 13:36:59 +0500

    try

--- shedule.js
+++ shedule.js
@@ -1,8 +1,12 @@
 const fetch = require('node-fetch');
 const schedule = async () => {
-    const r = await fetch('https://sheltered-reef-47163.herokuapp.com/');
-    const s = await r.json();
-    return s;
+    try {
+        const r = await fetch('https://sheltered-reef-47163.herokuapp.com/');
+        const s = await r.json();
+        return s;
+    } catch (e) {
+        return e;
+    }
 }
 
 const s = await schedule();
