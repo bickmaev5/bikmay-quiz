999a701ec6f9f512432bec5aa954bb12ab7da556
i.bikmaev <koshakk5@gmail.com>
2020-06-09 15:52:54 +0500

    add parser

--- parser.js
+++ parser.js
@@ -1,12 +1,67 @@
-const fetch = require('node-fetch');
+const dotenv = require('dotenv');
+dotenv.config();
+const request = require('request');
+const gql = require('graphql-request');
+const cheerio = require('cheerio');
+const API = process.env.HASURA_URL;
+const url = 'https://www.wowprogress.com/pve/ru';
 
-const parser = async () => {
-    const res = await fetch('https://www.wowprogress.com/pve/ru');
-    console.log(res);
-};
+const clearTop = `
+mutation clearRaidTop {
+    delete_raid_top(where: {}
+    ) {
+      affected_rows
+    }
+  }
+  
+`
 
-const main = async () => {
-    const result = await parser();
-};
+const insertTop = `
+mutation insert_raid_top($objects: [raid_top_insert_input!]!) {
+    insert_raid_top(objects: $objects) {
+      returning {
+        id
+      }
+    }
+}
+`;
 
-main();
+const data = [];
+
+request(url, async function (error, res, body) {
+    if (!error) {
+        const $ = cheerio.load(body);
+        const $ratings = $(".ratingContainer .rating tbody tr");
+        $ratings.each((index, row) => {
+            if (index !== 0) {
+                data[index - 1] = {};
+                const $row = $(row).children('td');
+                $row.each((i, cell) => {
+                    $cell = $(cell);
+                    switch (i) {
+                        case 0: 
+                            data[index - 1].place = $cell.children('span').text();
+                            break;
+                        case 1:
+                            data[index - 1].guildName = $cell.find("a nobr").text();
+                            const classList = $cell.children('a').attr('class').split(' ');
+                            data[index - 1].fraction = classList.includes('horde') ? 'horde' : 'alliance'; 
+                            break;
+                        case 2:
+                            data[index - 1].realm = $cell.find("a").text();
+                            break;
+                        case 3:
+                            data[index - 1].progress = $cell.find("span b").text();
+                            break;
+                        default:
+                            break;
+                    }
+                });
+            }
+        });
+        await gql.request(API, clearTop);
+        await gql.request(API, insertTop, {
+            objects: data
+        });
+    }
+});
